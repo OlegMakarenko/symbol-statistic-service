0a2ae6ecda414493124c680dbbe3da4e0639fe12
Oleh Makarenko <olegomm@gmail.com>
2020-02-14 14:03:57 +0200

    fix fetch block function

--- src/services/Http.js
+++ src/services/Http.js
@@ -14,19 +14,38 @@ class Infrastructure {
     }
 
     getBlocksFromHeightWithLimit = async (limit, fromBlockHeight) => {
+        console.log('Get block info from height:' + fromBlockHeight + 'with limit: ' + limit);
+
+        if(!limit || limit === 0)
+            limit = 100;
+
+        const apiCallsCount = Math.ceil(limit / 100);
+        let blocksData = [];
+
+        for(let i = 0; i < apiCallsCount; i++) {
+            const blocksDataPage = await this.fetchBlocksFromHeight(fromBlockHeight - 100 * i);
+            if(blocksDataPage)
+                blocksData = [ ...blocksData, ...blocksDataPage];
+        }
+        
+        return blocksData.slice(0, limit - 1)
+    }
+
+    fetchBlocksFromHeight = async (fromBlockHeight) => {
+        console.log('Fetch block data from height:', fromBlockHeight)
         let blockHeight
         if (fromBlockHeight === void 0)
             blockHeight = 'latest'
         else
             blockHeight = fromBlockHeight.toString()
     
-        const path = `/blocks/from/${blockHeight}/limit/${limit}`
+        const path = `/blocks/from/${blockHeight}/limit/100`
         const response = await axios.get(nodeUrl + path)
             .catch(err => { throw Error('Failed to get blocks', err) });
         const blocks = response.data.map(blockDTO => format.createBlockInfoFromDTO(blockDTO, this.repositoryFactory.networkType))
 
         return format.formatBlocks(blocks);
-      }
+    }
 }
 
 
